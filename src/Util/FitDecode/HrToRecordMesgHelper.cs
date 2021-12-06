943ed76f152c185b5fe9c586e78a8e8edad7d9d6
Sören Christ <soeren.christ@azo.com>
2021-12-06 13:13:38 +0100

    Refactor hr matching

--- src/Util/FitDecode/HrToRecordMesgHelper.cs
+++ src/Util/FitDecode/HrToRecordMesgHelper.cs
@@ -8,19 +8,16 @@ internal class HrToRecordMesgHelper
 {
     public static void MergeHeartRates(FitMessages messages)
     {
-        float? hr_anchor_event_timestamp = 0.0f;
-        DateTime hr_anchor_timestamp = new DateTime(0);
-        bool hr_anchor_set = false;
-        byte? last_valid_hr = 0;
-        DateTime last_valid_hr_time = new DateTime(0);
+        float? hrAnchorEventTimestamp = 0.0f;
+        DateTime hrAnchorTimestamp = new DateTime(0);
+        bool hrAnchorSet = false;
+        byte? lastValidHr = 0;
+        DateTime lastValidHrTime = new DateTime(0);
 
-        DateTime record_range_start_time = new DateTime(messages.Records[0].GetTimestamp());
-        int hr_start_index = 0;
-        int hr_start_sub_index = 0;
+        DateTime recordRangeStartTime = new DateTime(messages.Records[0].GetTimestamp());
+        int hrStartIndex = 0;
+        int hrSubIndex = 0;
 
-        //
-        // Update this foreach() to loop through just the Record messages
-        //
         foreach (RecordMesg recordMesg in messages.Records)
         {
             long hrSum = 0;
@@ -33,28 +30,30 @@ internal class HrToRecordMesgHelper
             bool findingInRangeHrMesgs = true;
 
             // Start searching HR mesgs where we left off
-            int hr_mesg_counter = hr_start_index;
-            int hr_sub_mesg_counter = hr_start_sub_index;
+            int hrMesgCounter = hrStartIndex;
+            int hrSubMesgCounter = hrSubIndex;
 
             //
             // Update this while() to loop through just the HR messages
             //
-            while (findingInRangeHrMesgs && (hr_mesg_counter < messages.HeartRates.Count))
+            while (findingInRangeHrMesgs && (hrMesgCounter < messages.HeartRates.Count))
             {
-                HrMesg hrMesg = new HrMesg(messages.HeartRates[hr_mesg_counter]);
+                HrMesg hrMesg = new HrMesg(messages.HeartRates[hrMesgCounter]);
 
                 // Update HR timestamp anchor, if present
                 if (hrMesg.GetTimestamp() != null && hrMesg.GetTimestamp().GetTimeStamp() != 0)
                 {
-                    hr_anchor_timestamp = new DateTime(hrMesg.GetTimestamp());
-                    hr_anchor_set = true;
+                    hrAnchorTimestamp = new DateTime(hrMesg.GetTimestamp());
+                    hrAnchorSet = true;
 
-                    if (hrMesg.GetFractionalTimestamp() != null)
-                        hr_anchor_timestamp.Add((double)hrMesg.GetFractionalTimestamp());
+                    var fractionalTimestamp = hrMesg.GetFractionalTimestamp();
+
+                    if (fractionalTimestamp != null)
+                        hrAnchorTimestamp.Add(fractionalTimestamp.Value);
 
                     if (hrMesg.GetNumEventTimestamp() == 1)
                     {
-                        hr_anchor_event_timestamp = hrMesg.GetEventTimestamp(0);
+                        hrAnchorEventTimestamp = hrMesg.GetEventTimestamp(0);
                     }
                     else
                     {
@@ -62,7 +61,7 @@ internal class HrToRecordMesgHelper
                     }
                 }
 
-                if (hr_anchor_set == false)
+                if (hrAnchorSet == false)
                 {
                     // We cannot process any HR messages if we have not received a timestamp anchor
                     throw new FitException("FIT HrToRecordMesgBroadcastPlugin Error: No anchor timestamp received in a HR mesg before diff HR mesgs");
@@ -71,57 +70,61 @@ internal class HrToRecordMesgHelper
                 {
                     throw new FitException("FIT HrToRecordMesgBroadcastPlugin Error: HR mesg with mismatching event timestamp and filtered bpm");
                 }
-                for (int j = hr_sub_mesg_counter; j < hrMesg.GetNumEventTimestamp(); j++)
+                for (int j = hrSubMesgCounter; j < hrMesg.GetNumEventTimestamp(); j++)
                 {
                     // Build up timestamp for each message using the anchor and event_timestamp
-                    DateTime hrMesgTime = new DateTime(hr_anchor_timestamp);
-                    float? event_timestamp = hrMesg.GetEventTimestamp(j);
+                    DateTime hrMesgTime = new DateTime(hrAnchorTimestamp);
+                    float? eventTimeStamp = hrMesg.GetEventTimestamp(j);
 
                     // Deal with roll over case
-                    if (event_timestamp < hr_anchor_event_timestamp)
+                    if (eventTimeStamp < hrAnchorEventTimestamp)
                     {
-                        if ((hr_anchor_event_timestamp - event_timestamp) > (1 << 21))
+                        if ((hrAnchorEventTimestamp - eventTimeStamp) > (1 << 21))
                         {
-                            event_timestamp += (1 << 22);
+                            eventTimeStamp += (1 << 22);
                         }
                         else
                         {
                             throw new FitException("FIT HrToRecordMesgBroadcastPlugin Error: Anchor event_timestamp is greater than subsequent event_timestamp. This does not allow for correct delta calculation.");
                         }
                     }
-                    hrMesgTime.Add((double)(event_timestamp - hr_anchor_event_timestamp));
+                    var diff = eventTimeStamp - hrAnchorEventTimestamp;
+                    if (diff != null)
+                        hrMesgTime.Add((double) diff);
 
                     // Check if hrMesgTime is gt record start time
                     // and if hrMesgTime is lte to record end time
-                    if ((hrMesgTime.CompareTo(record_range_start_time) > 0) &&
+                    if ((hrMesgTime.CompareTo(recordRangeStartTime) > 0) &&
                        (hrMesgTime.CompareTo(record_range_end_time) <= 0))
                     {
-                        hrSum += (long)hrMesg.GetFilteredBpm(j);
+                        var bpm = hrMesg.GetFilteredBpm(j);
+                        if (bpm != null)
+                            hrSum += (long)bpm;
                         hrSumCount++;
-                        last_valid_hr_time = new DateTime(hrMesgTime);
+                        lastValidHrTime = new DateTime(hrMesgTime);
 
                     }
                     // check if hrMesgTime exceeds the record time
                     else if (hrMesgTime.CompareTo(record_range_end_time) > 0)
                     {
                         // Remember where we left off
-                        hr_start_index = hr_mesg_counter;
-                        hr_start_sub_index = j;
+                        hrStartIndex = hrMesgCounter;
+                        hrSubIndex = j;
                         findingInRangeHrMesgs = false;
 
                         if (hrSumCount > 0)
                         {
                             // Update record heart rate
-                            last_valid_hr = (byte?)System.Math.Round((((float)hrSum) / hrSumCount), System.MidpointRounding.AwayFromZero);
-                            recordMesg.SetHeartRate(last_valid_hr);
+                            lastValidHr = (byte?)System.Math.Round((((float)hrSum) / hrSumCount), System.MidpointRounding.AwayFromZero);
+                            recordMesg.SetHeartRate(lastValidHr);
                             messages.RecordFieldNames.Add("HeartRate");
                         }
                         // If no stored HR is available, fill in record messages with the
                         // last valid filtered hr for a maximum of 5 seconds
-                        else if ((record_range_start_time.CompareTo(last_valid_hr_time) > 0) &&
-                                ((record_range_start_time.GetTimeStamp() - last_valid_hr_time.GetTimeStamp()) < 5))
+                        else if ((recordRangeStartTime.CompareTo(lastValidHrTime) > 0) &&
+                                ((recordRangeStartTime.GetTimeStamp() - lastValidHrTime.GetTimeStamp()) < 5))
                         {
-                            recordMesg.SetHeartRate(last_valid_hr);
+                            recordMesg.SetHeartRate(lastValidHr);
                             messages.RecordFieldNames.Add("HeartRate");
                         }
 
@@ -129,15 +132,15 @@ internal class HrToRecordMesgHelper
                         hrSum = 0;
                         hrSumCount = 0;
 
-                        record_range_start_time = new DateTime(record_range_end_time);
+                        recordRangeStartTime = new DateTime(record_range_end_time);
 
                         // Breaks out of looping within the event_timestamp array
                         break;
                     }
                 }
 
-                hr_mesg_counter++;
-                hr_sub_mesg_counter = 0;
+                hrMesgCounter++;
+                hrSubMesgCounter = 0;
             }
         }
     }
